bfd31e7d09205238f23ce4b256f49311c9d07982
Slava <rybco@outlook.com>
2022-06-23 15:01:47 +0300

    Adjust trace_ray method of raytracer class to traverse geometry and call a closest hit shader Add closest_hit_shader to raytracer class to return diffuse color

--- src/renderer/raytracer/raytracer.h
+++ src/renderer/raytracer/raytracer.h
@@ -112,6 +112,7 @@ namespace cg::renderer
 		std::shared_ptr<cg::resource<float3>> history;
 		std::vector<std::shared_ptr<cg::resource<unsigned int>>> index_buffers;
 		std::vector<std::shared_ptr<cg::resource<VB>>> vertex_buffers;
+		std::vector<triangle<VB>> triangles;
 
 		size_t width = 1920;
 		size_t height = 1080;
@@ -194,8 +195,28 @@ namespace cg::renderer
 			return miss_shader(ray);
 
 		depth--;
+		payload closest_hit_payload = {};
+		closest_hit_payload.t = max_t;
+		const triangle<VB>* closest_triangle = nullptr;
+
+		for(auto& triangle: triangles)
+		{
+			payload payload = intersection_shader(triangle, ray);
+			if (payload.t > min_t && payload.t < closest_hit_payload.t)
+			{
+				closest_hit_payload = payload;
+				closest_triangle = &triangle;
+
+				if(any_hit_shader)
+					return any_hit_shader(ray, payload, triangle);
+			}
+
+
+		}
+		if (closest_hit_shader && closest_hit_payload.t < max_t)
+			return closest_hit_shader(ray, closest_hit_payload, *closest_triangle, depth);
+
 		return miss_shader(ray);
-		// TODO: Lab 2.02. Adjust trace_ray method of raytracer class to traverse geometry and call a closest hit shader
 		// TODO: Lab 2.04. Adjust `trace_ray` method of `raytracer` to use `any_hit_shader`
 		// TODO: Lab 2.05. Adjust trace_ray method of raytracer class to traverse the acceleration structure
 	}
--- src/renderer/raytracer/raytracer_renderer.cpp
+++ src/renderer/raytracer/raytracer_renderer.cpp
@@ -32,6 +32,7 @@ void cg::renderer::ray_tracing_renderer::init()
 	raytracer->set_vertex_buffers(model->get_vertex_buffers());
 	raytracer->set_index_buffers(model->get_index_buffers());
 
+	lights.push_back({float3 {0.f, 1.58f, - 0.03f }, float3{0.78f, 0.78f, 0.78f}});
 
 	// TODO: Lab 2.03. Add light information to lights array of ray_tracing_renderer
 	// TODO: Lab 2.04. Initialize `shadow_raytracer` in `ray_tracing_renderer`
@@ -52,6 +53,28 @@ void cg::renderer::ray_tracing_renderer::render()
 		return payload;
 	};
 
+	raytracer->closest_hit_shader = [&](const ray& ray, payload& payload, const triangle<cg::vertex>& triangle, size_t depth) {
+		float3 position = ray.position + ray.direction * payload.t;
+		float3 normal = normalize(payload.bary.x * triangle.na +
+								  payload.bary.y * triangle.nb +
+								  payload.bary.z * triangle.nc);
+
+
+
+		float3 result_color = triangle.diffuse;
+
+		for(auto& light: lights)
+		{
+			cg::renderer::ray to_light(position, light.position - position);
+
+			result_color = triangle.diffuse * light.color * std::max(dot(normal, to_light.direction), 0.f);
+		}
+		payload.color = cg::color::from_float3(result_color);
+		return payload;
+	};
+
+	raytracer->build_acceleration_structure();
+
 	auto start = std::chrono::high_resolution_clock::now();
 	raytracer->ray_generation(
 			camera->get_position(), camera->get_direction(),
@@ -64,7 +87,6 @@ void cg::renderer::ray_tracing_renderer::render()
 	std::cout << "Raytracing took " << raytracing_duration.count() << " ms\n";
 
 	cg::utils::save_resource(*render_target, settings->result_path);
-	// TODO: Lab 2.02. Add closest_hit_shader to raytracer class to return diffuse color
 	// TODO: Lab 2.03. Adjust closest_hit_shader of raytracer to implement Lambertian shading model
 	// TODO: Lab 2.04. Define any_hit_shader and miss_shader for shadow_raytracer
 	// TODO: Lab 2.04. Adjust closest_hit_shader of raytracer to cast shadows rays and to ignore occluded lights
