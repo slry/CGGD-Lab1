a698e773112018998f73b85939a639d6bad1acb1
Slava <rybco@outlook.com>
2022-07-07 18:12:50 +0300

    Enable a validation layer

--- src/renderer/dx12/dx12_renderer.cpp
+++ src/renderer/dx12/dx12_renderer.cpp
@@ -2,7 +2,7 @@
 
 #include "utils/com_error_handler.h"
 #include "utils/window.h"
-
+#include <d3d12sdklayers.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
@@ -49,7 +49,16 @@ void cg::renderer::dx12_renderer::render()
 
 ComPtr<IDXGIFactory4> cg::renderer::dx12_renderer::get_dxgi_factory()
 {
-	// TODO Lab 3.02. Enable a validation layer
+	UINT dxgi_factory_flags = 0;
+#ifdef _DEBUG
+	//ComPtr<IDXGIFactory4>
+	ComPtr<ID3D12Debug> debug_controller;
+	if(SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debug_controller))))
+	{
+		debug_controller->EnableDebugLayer();
+		dxgi_factory_flags |= DXGI_CREATE_FACTORY_DEBUG;
+	}
+#endif
 	// TODO Lab 3.02. Create a DXGI factory
 }
 
