45467f31fdd685001a79026a3f08017434925551
Slava <rybco@outlook.com>
2022-07-07 20:02:23 +0300

    bug fix

--- src/renderer/dx12/dx12_renderer.cpp
+++ src/renderer/dx12/dx12_renderer.cpp
@@ -534,8 +534,53 @@ void cg::renderer::dx12_renderer::populate_command_list()
 	command_list->SetGraphicsRootSignature(root_signature.Get());
 	ID3D12DescriptorHeap* heaps[] = {cbv_srv_heap.get()};
 	command_list->SetDescriptorHeaps(_countof(heaps), heaps);
-	command_list->SetComputeRootDescriptorTable(0, cbv_srv_heap.get_gpu_descriptor_handle(0));
+	command_list->SetGraphicsRootDescriptorTable(0, cbv_srv_heap.get_gpu_descriptor_handle(0));
 	command_list->RSSetViewports(1,&view_port);
+	command_list->RSSetScissorRects(1, &scissor_rect);
+	command_list->ResourceBarrier(
+			1, &CD3DX12_RESOURCE_BARRIER::Transition(
+					   render_targets[frame_index].Get(),
+					   D3D12_RESOURCE_STATE_PRESENT,
+					   D3D12_RESOURCE_STATE_RENDER_TARGET));
+
+	//DRAWING
+
+	command_list->OMSetRenderTargets(
+			1,
+			&rtv_heap.get_cpu_descriptor_handle(frame_index),
+			FALSE,
+			nullptr);
+
+	const float clear_color[] = {0.f, 0.f, 0.f};
+
+	command_list->ClearRenderTargetView(
+			rtv_heap.get_cpu_descriptor_handle(frame_index),
+			clear_color,
+			0,
+			nullptr);
+
+	command_list->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	for (size_t s = 0; s < model->get_vertex_buffers().size(); s++)
+	{
+		command_list->IASetVertexBuffers(0,1,&vertex_buffer_views[s]);
+		command_list->IASetIndexBuffer(&index_buffer_views[s]);
+		command_list->DrawIndexedInstanced(
+				static_cast<UINT>(model->get_index_buffers()[s]->get_number_of_elements()),
+				1,
+				0,
+				0,
+				0);
+	}
+
+	command_list->ResourceBarrier(
+			1,
+			&CD3DX12_RESOURCE_BARRIER::Transition(
+					render_targets[frame_index].Get(),
+					D3D12_RESOURCE_STATE_RENDER_TARGET,
+					D3D12_RESOURCE_STATE_PRESENT));
+
+	THROW_IF_FAILED(command_list->Close());
 
 }
 
